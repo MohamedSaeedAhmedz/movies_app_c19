61cca85c77a684bc660e2e2a2f6439ee0be1d8cb
Mohamed-Elsayed2004 <mohamedelsayed20045@gmail.com>
2026-09-11 00:45:11 +0300

    feat: add Firebase authentication and Firestore services with user model

---
+++ lib/core/firebase/firebase_auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthService {
+  static final FirebaseAuth auth = FirebaseAuth.instance;
+
+  static Future<UserCredential> registerUser({
+    required String email,
+    required String password,
+  }) async {
+    UserCredential user = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return user;
+  }
+
+  static Future<void> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+}
