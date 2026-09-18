832f3ae55e023451a51175f839c10200cd6cdd06
Mohamed-Elsayed2004 <mohamedelsayed20045@gmail.com>
2026-09-18 08:13:00 +0300

    feat: Replace LoginButton with custom ElevatedButton for user login functionality

--- lib/features/Login/widgets/login_form.dart
+++ lib/features/Login/widgets/login_form.dart
@@ -1,10 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/firebase/firebase_auth.dart';
 import 'package:movies_app/core/localization/app_localizations.dart';
 import 'package:movies_app/core/resources/app_color.dart';
 import 'package:movies_app/core/resources/app_icon.dart';
 import 'package:movies_app/core/routes/AppRoutes.dart';
-import 'package:movies_app/features/Login/widgets/login_button.dart';
 import 'package:movies_app/widget/custom_text_form_field.dart';
+import 'package:movies_app/widget/show_snack_bar.dart';
 
 class LoginForm extends StatefulWidget {
   final GlobalKey<FormState> formKey;
@@ -15,6 +18,7 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  bool isLoading = false;
   bool isPasswordHidden = true;
   String email = '';
   String password = '';
@@ -121,10 +125,55 @@ class _LoginFormState extends State<LoginForm> {
           ),
           const SizedBox(height: 16),
 
-          LoginButton(
-            formKey: widget.formKey,
-            email: email,
-            password: password,
+          SizedBox(
+            width: double.infinity,
+            height: 55,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: MColors.yellow,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              onPressed: () async {
+                if (widget.formKey.currentState!.validate()) {
+                  try {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    await FirebaseAuthService.loginUser(
+                      email: email,
+                      password: password,
+                    );
+
+                    if (context.mounted) {
+                      Navigator.of(
+                        context,
+                      ).pushReplacementNamed(AppRoutes.homescreen);
+                    }
+                  } catch (e) {
+                    log(e.toString());
+                    if (context.mounted) showSnackBar(context, e.toString());
+                  } finally {
+                    if (mounted) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                    }
+                  }
+                }
+              },
+              child: isLoading
+                  ? CircularProgressIndicator(color: MColors.black)
+                  : Text(
+                      l10n.login,
+                      style: const TextStyle(
+                        color: MColors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+            ),
           ),
         ],
       ),
